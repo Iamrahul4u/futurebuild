c6eeb35ff50fad716ced3be91615cf3fdbabeeb2
Rahul Gupta <iamrahulgupta4u@gmail.com>
2024-08-29 22:41:54 +0530

    minor changes

--- components/loaders/SimplePageLoader.tsx
+++ components/loaders/SimplePageLoader.tsx
@@ -2,7 +2,7 @@ import { Loader2, LoaderIcon, LoaderPinwheel } from "lucide-react";
 
 export default function SimplePageLoader() {
   return (
-    <div className="duration-[1500ms] flex w-full animate-pulse items-center justify-center gap-2 ease-in-out">
+    <div className="flex w-full animate-pulse items-center justify-center gap-2 duration-1000 ease-in-out">
       <LoaderIcon className="mb-4 h-6 w-6 animate-spin" />
     </div>
   );
