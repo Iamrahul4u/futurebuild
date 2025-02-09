2e45d979d6eb0d67043cd217a8186bf77745ca1a
Rahul Gupta <iamrahulgupta4u@gmail.com>
2025-02-09 19:15:01 +0530

    Post css Optimization

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,25 @@
 /** @type {import('postcss-load-config').Config} */
+import purgecss from "@fullhuman/postcss-purgecss";
+import cssnano from "cssnano";
+
 const config = {
   plugins: {
     tailwindcss: {},
+    autoprefixer: {}, // Ensures better browser compatibility
+    ...(process.env.NODE_ENV === "production"
+      ? {
+          [purgecss({
+            content: [
+              "./pages/**/*.tsx",
+              "./components/**/*.tsx",
+              "./app/**/*.tsx",
+            ],
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          })]: {},
+          cssnano: { preset: "default" },
+        }
+      : {}),
   },
 };
 
