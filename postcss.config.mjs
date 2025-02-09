70961e582167e10114c2e9ba64c925acdfa4fa6b
Rahul Gupta <iamrahulgupta4u@gmail.com>
2025-02-09 19:26:36 +0530

    Post css Optimization

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,6 @@
 import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
-import purgecss from "@fullhuman/postcss-purgecss";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -10,17 +9,8 @@ const config = {
   plugins: [
     tailwindcss,
     autoprefixer,
-    isProduction &&
-      purgecss({
-        content: [
-          "./pages/**/*.tsx",
-          "./components/**/*.tsx",
-          "./app/**/*.tsx",
-        ],
-        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }),
-    isProduction && cssnano({ preset: "default" }),
-  ].filter(Boolean), // Removes `false` values in dev mode
+    isProduction && cssnano({ preset: "default" }), // Minifies CSS only in production
+  ].filter(Boolean),
 };
 
 export default config;
